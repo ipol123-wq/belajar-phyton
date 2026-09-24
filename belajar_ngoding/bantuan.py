130c279254da1b3e3a0cf70afa3bd72aaa78975f
ipol123-wq <widodosultan24@gmail.com>
2026-09-24 17:08:55 +0800

    web Flask ATM

--- belajar_ngoding/bantuan.py
+++ belajar_ngoding/bantuan.py
@@ -1,3 +1,121 @@
-print('aku sedang belajar git')
-print('udah lumayan bisa aku cik')
-print('ok sih wak')
+import os
+import json
+from flask import Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
+app.secret_key='rahasia123'
+pin_benar='654321'
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    pesan=''
+    if request.method=='POST':
+        pin = request.form.get('pin')
+        if pin==pin_benar:
+            session['login']=True
+            return redirect(url_for('menu'))
+        else:
+            pesan = 'pin salah'
+    return render_template('login.html', pesan=pesan)
+@app.route('/menu')
+def menu():
+    if not session.get('login'):
+        return redirect(url_for('menu'))
+    return render_template('menu.html')
+@app.route('/saldo')
+def saldo():
+    if not session.get('login'):
+        return redirect(url_for('home'))
+    if os.path.exists('saldo.txt'):
+        with open ('saldo.txt', 'r') as file:
+            saldo_user=int(file.read())
+    else:
+        saldo_user=0
+    return render_template('saldo.html', saldo=saldo_user)
+
+@app.route('/setor', methods=['GET', 'POST'])
+def setor():
+    if not session.get('login'):
+        return redirect(url_for('home'))
+    pesan = ''
+    if request.method=='POST':
+        try:
+            jumlah = int(request.form.get('jumlah'))
+        except:
+            pesan=f'masukkan nominal woi'
+            return render_template('setor.html', pesan=pesan)
+        if os.path.exists('saldo.txt'):
+            with open ('saldo.txt', 'r') as file:
+                saldo_user=int(file.read())
+        else:
+            saldo_user=0
+        saldo_user+=jumlah
+        with open('saldo.txt', 'w') as file:
+            file.write(str(saldo_user))
+        pesan = f'{jumlah} berhasil di setor'
+        if os.path.exists('riwayat.json'):
+            with open('riwayat.json', 'r') as file:
+                riwayat = json.load(file)
+        else:
+            riwayat = []
+        riwayat.append({
+            'jenis': 'pemasukan',
+            'jumlah': jumlah
+        })
+        with open ('riwayat.json', 'w') as file:
+            json.dump(riwayat, file)
+            pesan=f'{jumlah} berhasil di setor'
+        return render_template('setor.html', pesan=pesan)
+    return render_template ('setor.html', pesan=pesan)
+
+@app.route('/tarik', methods=['GET', 'POST'])
+def tarik():
+    if not session.get('login'):
+        return redirect(url_for('home'))
+    pesan =''
+    if request.method=='POST':
+        try:
+            jumlah=int(request.form.get('jumlah'))
+        except:
+            pesan=f'masukin angka woilah cik'
+            return render_template('tarik.html', pesan=pesan)
+        if os.path.exists('saldo.txt'):
+            with open ('saldo.txt', 'r') as file:
+                saldo_user=int(file.read())
+        else:
+            saldo_user=0
+        if os.path.exists('riwayat.json'):
+            with open('riwayat.json', 'r') as file:
+                riwayat = json.load(file)
+        else:
+            riwayat = []
+        if jumlah <= saldo_user:
+            saldo_user -= jumlah
+            with open('saldo.txt', 'w') as file:
+                file.write(str(saldo_user))
+            pesan = f'{jumlah} berhasil di tarik'
+            riwayat.append({
+                'jenis': 'pengeluaran',
+                'jumlah': jumlah
+            })
+            with open ('riwayat.json', 'w') as file:
+                json.dump(riwayat, file)
+        else:
+            pesan = 'saldo anda tidak mencukupi'
+        return render_template('tarik.html', pesan=pesan)
+    return render_template('tarik.html', pesan=pesan)
+@app.route('/riwayat')
+def riwayat():
+    if not session.get('login'):
+        return redirect(url_for('home'))
+    if os.path.exists('riwayat.json'):
+        with open ('riwayat.json', 'r') as file:
+            data = json.load(file)
+    else:
+        data = []
+    return render_template('riwayat.html', riwayat=data)
+@app.route('/logout')
+def logout():
+    session.pop('login', None)
+    return redirect(url_for('home'))
+if __name__=='__main__':
+    app.run(debug=True)
+
--- belajar_ngoding/bantuan2.py
+++ belajar_ngoding/bantuan2.py
@@ -3,89 +3,87 @@ import json
 from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key='rahasia123'
-pin_benar='654321'
+pin_benar = '654321'
 def baca_saldo():
     if os.path.exists('saldo.txt'):
-        with open ('saldo.txt', 'r') as file:
+        with open('saldo.txt', 'r') as file:
             return int(file.read())
-    return  0
+    return 0
 def simpan_saldo(saldo_user):
-    with open('saldo.txt', 'w') as file:
+    with open ('saldo.txt', 'w') as file:
         file.write(str(saldo_user))
 def baca_riwayat():
     if os.path.exists('riwayat.json'):
         with open('riwayat.json', 'r') as file:
-            isi = file.read().strip()
-            if isi =='':
-                return []
-            return json.loads(isi)
+            riwayat = json.load(file)
+            return riwayat
     return []
 def simpan_riwayat(riwayat):
-    with open('riwayat.json', 'w') as file:
-        json.dump(riwayat, file)
+    with open ('riwayat.json', 'w') as file:
+        json.dump (riwayat, file)
 def wajib_login():
     return not session.get('login')
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    pesan=''
+    pesan = ''
     if request.method=='POST':
         pin = request.form.get('pin')
-        if pin==pin_benar:
+        if pin == pin_benar:
             session['login']=True
             return redirect(url_for('menu'))
         else:
             pesan = 'pin salah'
-    return render_template('login.html', pesan=pesan)
+    return render_template('login.html', pesan = pesan)
 @app.route('/menu')
 def menu():
     if wajib_login():
-        return redirect(url_for('menu'))
+        return redirect(url_for('login'))
     return render_template('menu.html')
 @app.route('/saldo')
 def saldo():
     if wajib_login():
         return redirect(url_for('menu'))
     return render_template('saldo.html', saldo=baca_saldo())
-
 @app.route('/setor', methods=['GET', 'POST'])
 def setor():
     if wajib_login():
-        return redirect(url_for('menu'))
-    pesan=''
+        return redirect(url_for('login'))
+    pesan = ''
     if request.method=='POST':
         try:
             jumlah = int(request.form.get('jumlah'))
         except:
-            pesan='nominal harus angka woilah'
+            pesan = 'masukan anga woilah'
             return render_template('setor.html', pesan=pesan)
         saldo_user=baca_saldo()
-        saldo_user+=jumlah
+        saldo_user += jumlah
         simpan_saldo(saldo_user)
-        riwayat=baca_riwayat()
+        riwayat = baca_riwayat()
         riwayat.append({
             'jenis': 'pemasukan',
             'jumlah': jumlah
         })
         simpan_riwayat(riwayat)
-        pesan =f'{jumlah} berhasil di setor'
+        pesan = f'{jumlah} berhasil di setor'
         return render_template('setor.html', pesan=pesan)
     return render_template('setor.html', pesan=pesan)
 @app.route('/tarik', methods=['GET', 'POST'])
 def tarik():
-    if wajib_login():
-        return redirect(url_for('menu'))
     pesan=''
+    if wajib_login():
+        return redirect(url_for('login'))
     if request.method=='POST':
         try:
             jumlah = int(request.form.get('jumlah'))
         except:
-            pesan = 'masukan angka woilah cik'
+            pesan = 'masukin angak woi'
             return render_template('tarik.html', pesan=pesan)
         saldo_user = baca_saldo()
         if jumlah <= saldo_user:
             saldo_user -= jumlah
             simpan_saldo(saldo_user)
-            riwayat=baca_riwayat()
+            riwayat = baca_riwayat()
             riwayat.append({
                 'jenis': 'pengeluaran',
                 'jumlah': jumlah
@@ -93,17 +91,21 @@ def tarik():
             simpan_riwayat(riwayat)
             pesan = f'{jumlah} berhasil di tarik'
         else:
-            pesan = 'saldo anda tidak mencukupi'
+            pesan = 'mohon maaf saldo anda tidak mencukupi'
         return render_template('tarik.html', pesan=pesan)
     return render_template('tarik.html', pesan=pesan)
 @app.route('/riwayat')
 def riwayat():
     if wajib_login():
-        return redirect(url_for('menu'))
+        return redirect(url_for('login'))
     return render_template('riwayat.html', riwayat=baca_riwayat())
 @app.route('/logout')
 def logout():
     session.pop('login', None)
     return redirect(url_for('home'))
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     app.run(debug=True)
+
+
